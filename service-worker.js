b207fc90ee49b6d85ab77710473842e7ea43861d
MadKiwi98 <167824528+MadKiwi98@users.noreply.github.com>
2024-11-05 23:17:52 +1300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'dairy-shed-checklist-cache-v2';
-const DATA_CACHE_NAME = 'data-cache-v2';
+const CACHE_NAME = 'dairy-shed-checklist-cache-v3';
+const DATA_CACHE_NAME = 'data-cache-v3';
 const urlsToCache = [
     '/',
     '/index.html',
