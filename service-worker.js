dec3be538f4765c97a251e30776dc89298377fe1
MadKiwi98 <167824528+MadKiwi98@users.noreply.github.com>
2024-11-06 13:12:34 +1300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,51 @@
-const CACHE_NAME = 'dairy-shed-checklist-cache';
+const CACHE_NAME = 'dairy-shed-cache-v1';
 const urlsToCache = [
-    'https://madkiwi98.github.io/test/',
-    'https://madkiwi98.github.io/test/index.html'
+    '/',
+    '/test/', // Adjusted to include '/test/' since your site is hosted under '/test/'
+    '/test/index.html',
+    // Include other assets relative to the root
+    'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/dexie/3.0.3/dexie.min.js',
+    'https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js',
+    'https://www.gstatic.com/firebasejs/9.15.0/firebase-storage-compat.js',
+    'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore-compat.js',
+    'https://i.postimg.cc/htZPjx5g/logo-89.png',
+    // Add any other assets or URLs that need to be cached
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+// Use a network-first strategy for fetch events
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request).then((response) => {
-                    return caches.open(CACHE_NAME).then((cache) => {
-                        cache.put(event.request, response.clone());
-                        return response;
-                    });
-                });
-            }).catch(() => {
-                return caches.match('index.html');
-            })
+        fetch(event.request).catch(function() {
+            return caches.match(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys()
+        .then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
