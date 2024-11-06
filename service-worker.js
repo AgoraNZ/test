eb0dbecbaba44c471f8caabf5b5e69d236a908a1
MadKiwi98 <167824528+MadKiwi98@users.noreply.github.com>
2024-11-06 13:09:54 +1300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,31 @@
-const CACHE_NAME = 'dairy-shed-cache-v1';
+const CACHE_NAME = 'dairy-shed-checklist-cache';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css', // If you have a separate CSS file
-    '/app.js', // If you have a separate JavaScript file
-    'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js',
-    'https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js',
-    'https://www.gstatic.com/firebasejs/9.15.0/firebase-storage-compat.js',
-    'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore-compat.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/dexie/3.0.3/dexie.min.js',
-    'https://i.postimg.cc/htZPjx5g/logo-89.png', // Logo used in the app
-    // Add any other assets or URLs that need to be cached
+    'https://madkiwi98.github.io/test/',
+    'https://madkiwi98.github.io/test/index.html'
 ];
 
-// Install event
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then(function(cache) {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-// Fetch event
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-        .then(function(response) {
-            // Return the cached version if available, else fetch from network
-            return response || fetch(event.request);
-        })
-    );
-});
-
-// Activate event
-self.addEventListener('activate', function(event) {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys()
-        .then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
+            .then((response) => {
+                return response || fetch(event.request).then((response) => {
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    });
+                });
+            }).catch(() => {
+                return caches.match('index.html');
+            })
     );
 });
