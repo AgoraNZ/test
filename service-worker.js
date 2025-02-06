33220fc69a663fdfd7c73fc2de47976049ad099c
MadKiwi98 <167824528+MadKiwi98@users.noreply.github.com>
2025-02-07 11:55:01 +1300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump the cache name version each time you release a new update:
-const CACHE_NAME = 'dairy-shed-cache-v48';  // <--- increment for new version
+const CACHE_NAME = 'dairy-shed-cache-v49';  // <--- increment for new version
 
 const urlsToCache = [
     '/',
